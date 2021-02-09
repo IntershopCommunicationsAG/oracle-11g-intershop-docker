e38c6067cb8b57024e678b49631aebce090cc81f
Matthias Raab <m.raab@intershop.de>
2021-02-09 11:36:05 +0100

    try to solve startup

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+monitor() {
+    tail -F -n 0 $1 | while read line; do echo -e "$2: $line"; done
+}
+
 LISTENER_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 TNSNAMES_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
+
+ORACLE_BASE=/u01/app/oracle
+ALERT_LOG="$ORACLE_BASE/diag/rdbms/xe/XE/trace/alert_XE.log"
+LISTENER_LOG="$ORACLE_BASE/diag/tnslsnr/$HOSTNAME/listener/trace/listener.log"
 
 cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA" && 
 sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}" && 
@@ -9,4 +18,6 @@ cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA" &&
 sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}" &&
 sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}" &&
 
-service oracle-xe start
+service oracle-xe start &&
+
+monitor $ALERT_LOG
